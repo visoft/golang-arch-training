e609f521a83efeb64c23c95336d78ff58f3e986c
Damien White <damien.white@visoftinc.com>
2020-09-28 20:03:34 -0400

    Adds marshal example (#14)

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Person struct {
+	First string
+}
+
+func main() {
+	p1 := Person{
+		First: "Damien",
+	}
+	p2 := Person{
+		First: "Dave",
+	}
+	xp := []Person{p1, p2}
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(string(bs))
+}
